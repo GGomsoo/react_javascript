3ba2f285f94a70a6492d60a1f785124e043a6f3e
Park Jong Hyuk <occultism22@naver.com>
2024-10-04 18:52:16 +0900

    섹션4: 리액트 핵심 - 심화 단계 91. 틱택토 게임: "게임 오버" 화면 & 무승부 여부 확인

--- section_4/tic-tac-toe/src/App.jsx
+++ section_4/tic-tac-toe/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import GameOver from "./components/GameOver";
+
+import { WINNING_COMBINATIONS } from "./winning-combinations";
 
 const deriveActivePlayer = (gameTurns) => {
   let currentPlayer = "X";
@@ -13,11 +16,50 @@ const deriveActivePlayer = (gameTurns) => {
   return currentPlayer;
 };
 
-const App = () => {
-  // const [activePlayer, setActivePlayer] = useState("X");
-  const [gameTurns, setGameTurns] = useState([]);
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
-  const activePlayer = deriveActivePlayer(gameTurns)
+const App = () => {
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState("X");
+  // const [Winner, setWinner] = useState(false)
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  // gameBoard는 계산된 값
+  // 어떠한 상태에서 파생된 것 (App.jsx의 gameTurns)
+  let gameBoard = initialGameBoard;
+
+  // 제어하는 상태의 수는 최소화하면서
+  // 가능한 많은 정보와 많은 값을 파생시키는 것
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  // 우승 조건
+  let winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].col];
+    const thridSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thridSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   // 게임 순서
   // 최근 순서가 X 였으면, 이번엔 O
@@ -52,7 +94,8 @@ const App = () => {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onPlayer={handlePlayer} turns={gameTurns} />
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        <GameBoard onPlayer={handlePlayer} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- section_4/tic-tac-toe/src/components/GameBoard.jsx
+++ section_4/tic-tac-toe/src/components/GameBoard.jsx
@@ -1,22 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-const GameBoard = ({ onPlayer, turns }) => {
-  // gameBoard는 계산된 값
-  // 어떠한 상태에서 파생된 것 (App.jsx의 gameTurns)
-  let gameBoard = initialGameBoard;
 
-  // 제어하는 상태의 수는 최소화하면서
-  // 가능한 많은 정보와 많은 값을 파생시키는 것
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
+const GameBoard = ({ onPlayer, board }) => {
+  
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   // const handleChangeBoard = (rowIdx, colIdx) => {
@@ -31,7 +16,7 @@ const GameBoard = ({ onPlayer, turns }) => {
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIdx) => (
+      {board.map((row, rowIdx) => (
         <li key={rowIdx}>
           <ol>
             {row.map((playerSymbol, colIdx) => (
