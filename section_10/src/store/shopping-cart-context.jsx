a7a5b2f93270dc28b296987407012368196165ba
Park Jong Hyuk <occultism22@naver.com>
2024-11-23 15:28:13 +0900

    174. Action 보내기 & useReducer로 State(상태) 수정하기

--- section_10/src/store/shopping-cart-context.jsx
+++ section_10/src/store/shopping-cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useReducer } from "react";
+import { createContext, useReducer } from "react";
 import { DUMMY_PRODUCTS } from "../dummy-products.js";
 
 export const CartContext = createContext({
@@ -7,7 +7,64 @@ export const CartContext = createContext({
   updateItemQuantity: () => {},
 });
 
+// Reducer의 기능
 const shoppingCartReducer = (state, action) => {
+  // 장바구니에 추가하는 경우
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
+      updatedItems.push({
+        id: action.payload,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      // 값이 여러개라면, 다른 값을 잃어버리지 않도록 설정
+      ...state,
+      items: updatedItems,
+    };
+  }
+
+  // 장바구니 수량 변경하는 경우
+  if (action.type === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === action.payload.productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += action.payload.amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        ...state,
+        items: updatedItems,
+      };
+  }
   return state;
 };
 
@@ -19,67 +76,25 @@ const CartContextProvider = ({ children }) => {
     }
   );
 
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
-
   // 장바구니 추가
   function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
+    // useReducer의 action
+    shoppingCartDispatch({
+      type: "ADD_ITEM",
+      payload: id, // 함수의 매개변수로 받는 id를 payload로 설정
     });
   }
 
   // 장바구니 수량 수정
   function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
+    shoppingCartDispatch({
+      type: "UPDATE_ITEM",
+      payload: {
+        productId,
+        amount,
       }
-
-      return {
-        items: updatedItems,
-      };
     });
-  }
+  };
 
   const ctxValue = {
     items: shoppingCartState.items,
