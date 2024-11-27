4738a91e6af97d217d4b59889c1a0b2b8f89cba2
Park Jong Hyuk <occultism22@naver.com>
2024-11-27 18:57:31 +0900

    213. Key(키)를 사용한 컴포넌트 초기화

--- section_13/src/App.jsx
+++ section_13/src/App.jsx
@@ -18,7 +18,12 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSet={handleSetCount}/>
-        <Counter initialCount={chosenCount} />
+
+        {/* set Counter에 숫자를 입력 후 set을 하게 되면 */}
+        {/* 문구의 숫자는 변하지만, 증감 버튼 사이의 숫자는 변하지 않는다 */}
+        {/* 이를 변하게 하기 위해 Counter 컴포넌트에 key값으로 chosenCount를 전달 */}
+        {/* key 값이 바뀔 때 마다 choseCount의 상태도 변한다 */}
+        <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
