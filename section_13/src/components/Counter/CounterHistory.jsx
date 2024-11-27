5f019bb1399e7c4185f8ec8f8a5dc18fdcf6a502
Park Jong Hyuk <occultism22@naver.com>
2024-11-27 18:52:06 +0900

    212. Key(키)가 중요한 이유 부가 설명

--- section_13/src/components/Counter/CounterHistory.jsx
+++ section_13/src/components/Counter/CounterHistory.jsx
@@ -24,6 +24,10 @@ export default function CounterHistory({ history }) {
   return (
     <ol>
       {/* 특정 값과 연결된 속성 값을 key 값으로 사용해야 한다. */}
+      {/* index를 key로 사용하게 될 경우, 
+      증감 버튼을 누를 때 마다 historyItem 배열 전체에 변화를 준다. */}
+      {/* count의 id값을 key로 사용할 경우, 배열에 새로운 값만 추가될 뿐
+      배열 전체에 변화를 주진 않는다. */}
       {history.map((count) => (
         <HistoryItem key={count.id} count={count.value} />
       ))}
