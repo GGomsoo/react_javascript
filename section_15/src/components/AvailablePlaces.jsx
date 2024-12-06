cb8d4b426e2847e26d843a39d40e1173f5179b80
Park Jong Hyuk <occultism22@naver.com>
2024-12-06 21:20:50 +0900

    234. HTTP 에러 다루기

--- section_15/src/components/AvailablePlaces.jsx
+++ section_15/src/components/AvailablePlaces.jsx
@@ -1,22 +1,41 @@
+import Error from './Error.jsx';
 import Places from './Places.jsx';
 import { useEffect, useState } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, seteIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   // async, await, useEffect 이용한 비동기 API 통신
   useEffect(() => {
     const fetchPlaces = async () => {
       seteIsFetching(true)
-      const response = await fetch("http://localhost:3000/places")      
-      const resData = await response.json()
-      setAvailablePlaces(resData.places)
+      try {
+        const response = await fetch("http://localhost:3000/places")      
+        const resData = await response.json()
+        // fetch를 통한 HTTP
+        if (!response.ok) {
+          const err = new Error("failed to fetch places")
+          throw err
+        }
+        setAvailablePlaces(resData.places)
+      }
+      catch (error) {
+        setError({
+          message: error.message || "Could not fetch places, please try again later."
+        });
+      }
+      
       seteIsFetching(false)
     }
 
     fetchPlaces()
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message}/>
+  };
 
   return (
     <Places
