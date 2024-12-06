e48f8ace460417435b19866ec3e34b17162b45cf
Park Jong Hyuk <occultism22@naver.com>
2024-12-06 21:11:48 +0900

    233. 로딩 State(상태) 다루기

--- section_15/src/components/AvailablePlaces.jsx
+++ section_15/src/components/AvailablePlaces.jsx
@@ -3,13 +3,16 @@ import { useEffect, useState } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, seteIsFetching] = useState(false);
 
   // async, await, useEffect 이용한 비동기 API 통신
   useEffect(() => {
     const fetchPlaces = async () => {
+      seteIsFetching(true)
       const response = await fetch("http://localhost:3000/places")      
       const resData = await response.json()
       setAvailablePlaces(resData.places)
+      seteIsFetching(false)
     }
 
     fetchPlaces()
@@ -19,6 +22,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
