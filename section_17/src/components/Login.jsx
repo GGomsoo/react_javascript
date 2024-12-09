0d5c011657ac6f293b1c5b3ee63ca65e6fe59942
Park Jong Hyuk <occultism22@naver.com>
2024-12-09 17:19:06 +0900

    251. State(상태) & 일반 Handler로 사용자 입력 수집 및 관리

--- section_17/src/components/Login.jsx
+++ section_17/src/components/Login.jsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [entered, setEntered] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    // React 에서의 양식 제출 관리 방법
+    // 브라우저의 기본 구성이 일어나지 않게 한다
+    e.preventDefault();
+    console.log(entered)
+  };
+
+  const handleChangeInput = (identifier, value) => {
+    setEntered(prevValue => ({
+      ...prevValue,
+      [identifier]: value,
+    }))
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(e) => handleChangeInput("email", e.target.value)} value={entered.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(e) => handleChangeInput("password", e.target.value)} value={entered.password} />
         </div>
       </div>
 
@@ -21,4 +42,4 @@ export default function Login() {
       </p>
     </form>
   );
-}
+};
