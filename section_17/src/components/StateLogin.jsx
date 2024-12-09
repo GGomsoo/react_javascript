87abdae679ff59568c02e7b0ca32870678054622
Park Jong Hyuk <occultism22@naver.com>
2024-12-09 20:35:23 +0900

    255. State(상태)로 매 키보드 입력마다 유효성 검사하기

--- section_17/src/components/StateLogin.jsx
+++ section_17/src/components/StateLogin.jsx
@@ -6,25 +6,29 @@ export default function Login() {
     password: "",
   });
 
+  // 이메일에 @가 포함되어 있는지에 따라 true, false
+  // 없으면 true
+  const emailIsInvalid = entered.email !== "" && !entered.email.includes("@");
+
   const handleSubmit = (e) => {
     // React 에서의 양식 제출 관리 방법
     // 브라우저의 기본 구성이 일어나지 않게 한다
     e.preventDefault();
-    console.log(entered)
+    console.log(entered);
 
     // 상태를 통해 입력값을 관리할 때, 초기화 하는 방법
     setEntered({
       email: "",
       password: "",
-    })
+    });
   };
 
   const handleChangeInput = (identifier, value) => {
-    setEntered(prevValue => ({
+    setEntered((prevValue) => ({
       ...prevValue,
       [identifier]: value,
-    }))
-  }
+    }));
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -33,12 +37,25 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={(e) => handleChangeInput("email", e.target.value)} value={entered.email}/>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => handleChangeInput("email", e.target.value)}
+            value={entered.email}
+          />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={(e) => handleChangeInput("password", e.target.value)} value={entered.password} />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => handleChangeInput("password", e.target.value)}
+            value={entered.password}
+          />
         </div>
       </div>
 
@@ -48,4 +65,4 @@ export default function Login() {
       </p>
     </form>
   );
-};
+}
