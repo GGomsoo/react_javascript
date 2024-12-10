2bde31d6a8fc447253f2d29882a51da4296adb3a
Park Jong Hyuk <occultism22@naver.com>
2024-12-10 22:46:23 +0900

    312. 리덕스와 함께 useEffect 사용하기

--- section_20/src/App.js
+++ section_20/src/App.js
@@ -1,3 +1,4 @@
+// import { useEffect } from "react";
 import { useSelector } from "react-redux"
 
 import Cart from './components/Cart/Cart';
@@ -5,7 +6,17 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible)
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  // const cart = useSelector(state => state.cart);
+
+  // // Redux와 Back-end 통신
+  // // 강의에선 firebase와 통신을 예시로 든다
+  // // 장바구니가 변경 될 때 마다 useEffect 함수가 실행된다
+  // // Back-end 서버가 따로 있다면, API 통신으로 하면 된다
+  // useEffect(() => {
+  //   fetch("firebase_URL/cart.json", {method: "PUT", body: JSON.stringify(cart),})
+  // }, [cart])
+  
   return (
     <Layout>
       {showCart && <Cart />}
--- section_20/src/components/Shop/ProductItem.js
+++ section_20/src/components/Shop/ProductItem.js
@@ -6,6 +6,7 @@ import { useDispatch } from "react-redux"
 const ProductItem = (props) => {
   const dispatch = useDispatch();
   const { title, price, description, id } = props;
+
   const handleAddToCart = () => {
     dispatch(cartActions.addItemToCart({
       id,
