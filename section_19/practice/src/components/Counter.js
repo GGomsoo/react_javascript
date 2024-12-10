5f8a0ac8c956fdaa7ea0194c3f4890edb3b80c37
Park Jong Hyuk <occultism22@naver.com>
2024-12-10 15:57:55 +0900

    292. 내부 컴포넌트에서 Action을 Dispatch 하기

--- section_19/practice/src/components/Counter.js
+++ section_19/practice/src/components/Counter.js
@@ -1,9 +1,20 @@
 // useStore: 저장소에 직접 액세스 할 수 있다.
 // useSelector: 저장소가 관리하는 상태 부분을 우리가 자동으로 선택할 수 있다 <- 조금 더 편리
-import { useSelector, connect } from "react-redux";
+import { useSelector, connect, useDispatch } from "react-redux";
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // Redux store에 대한 action을 보내는 dispatch
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({type: "increment"})
+  };
+
+  const handleDecrement = () => {
+    dispatch({type: "decrement"})
+  };
+
   // useSelector를 통해
   // 해당 컴포넌트에 필요한 상태를 자동으로 구독
   const counter = useSelector(state => state.counter);
@@ -13,6 +24,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
