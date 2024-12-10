23f31ce0e40d297b219359a07333c0d848b9b1c9
Park Jong Hyuk <occultism22@naver.com>
2024-12-10 16:27:05 +0900

    295. 여러 State 속성 작업하기

--- section_19/practice/src/components/Counter.js
+++ section_19/practice/src/components/Counter.js
@@ -2,8 +2,11 @@
 // useSelector: 저장소가 관리하는 상태 부분을 우리가 자동으로 선택할 수 있다 <- 조금 더 편리
 import { useSelector, useDispatch } from "react-redux";
 import classes from './Counter.module.css';
+import { useState } from "react";
 
 const Counter = () => {
+  const [] = useState();
+
   // Redux store에 대한 action을 보내는 dispatch
   const dispatch = useDispatch();
 
@@ -22,17 +25,19 @@ const Counter = () => {
   // useSelector를 통해
   // 해당 컴포넌트에 필요한 상태를 자동으로 구독
   const counter = useSelector(state => state.counter);
-  const toggleCounterHandler = () => {};
+  const show = useSelector(state => state.showCounter);
+  const toggleCounterHandler = () => {
+    dispatch({type: "toggle"})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleIncrease}>+5</button>
         <button onClick={handleDecrement}>-</button>
-        <button onClick={handleDecrement}>-5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
