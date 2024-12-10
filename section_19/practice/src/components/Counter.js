49e9f200056ac67f58722d9d278b30c99b4f7156
Park Jong Hyuk <occultism22@naver.com>
2024-12-10 16:19:51 +0900

    294. 작업에 페이로드 연결하기

--- section_19/practice/src/components/Counter.js
+++ section_19/practice/src/components/Counter.js
@@ -1,6 +1,6 @@
 // useStore: 저장소에 직접 액세스 할 수 있다.
 // useSelector: 저장소가 관리하는 상태 부분을 우리가 자동으로 선택할 수 있다 <- 조금 더 편리
-import { useSelector, connect, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from './Counter.module.css';
 
 const Counter = () => {
@@ -9,6 +9,10 @@ const Counter = () => {
 
   const handleIncrement = () => {
     dispatch({type: "increment"})
+  };
+
+  const handleIncrease = () => {
+    dispatch({type: "increase", amount: 5})
   };
 
   const handleDecrement = () => {
@@ -26,7 +30,9 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={handleIncrement}>+</button>
+        <button onClick={handleIncrease}>+5</button>
         <button onClick={handleDecrement}>-</button>
+        <button onClick={handleDecrement}>-5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
