2fa0d9aa8c0477a67adcdd531cfd1dc170e08702
Park Jong Hyuk <occultism22@naver.com>
2024-12-11 13:27:00 +0900

    377. 자동 로그아웃 추가하기

--- section_22/frontend/src/pages/Root.js
+++ section_22/frontend/src/pages/Root.js
@@ -1,9 +1,23 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
 
 function RootLayout() {
+  const token = useLoaderData()
+  const submit = useSubmit();
   // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    // 토큰이 있는 경우, 타이머가 작동
+    // 1시간 뒤 자동 로그아웃
+    setTimeout(() => {
+      submit(null, {action: "/logout", method: "post"})
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
 
   return (
     <>
