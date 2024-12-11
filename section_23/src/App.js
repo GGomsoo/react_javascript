3d2f0b7b9e1873348f391d584f9d95120a611784
Park Jong Hyuk <occultism22@naver.com>
2024-12-11 14:08:24 +0900

    382. 지연 로딩 이해하기 383. 지연 로딩 추가하기 384. 프로덕션용 코드 빌드하기

--- section_23/src/App.js
+++ section_23/src/App.js
@@ -1,9 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
+// import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
+import { lazy, Suspense } from 'react';
+
+// lazy 함수는 실행될 때 동적으로  import하는 함수를 인자로 받는다
+const BlogPage = lazy(() => import("./pages/Blog"))
+const PostPage = lazy(() => import("./pages/Post"))
 
 const router = createBrowserRouter([
   {
@@ -17,8 +22,24 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          // BlogPage를 방문해야먄 해당 loader 가 import 되면서 '지연 로딩' 테크닉이 적용된다
+          // Suspense 컴포넌트: 다른 컴포넌트에 사용하여 로딩을 기다리는데 사용할 수 있다
+          {
+            index: true,
+            element:
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>,
+            loader: () => import("./pages/Blog").then(module => module.loader())
+          },
+          {
+            path: ':id',
+            element:
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>,
+            loader: (meta) => import("./pages/Post").then(module => module.loader(meta))
+          },
         ],
       },
     ],
