b700770bf6dcd0f5bbede81874772e30da9eadeb
Park Jong Hyuk <occultism22@naver.com>
2024-12-13 19:21:38 +0900

    541. 테스트 스위트와 함께 테스트 그룹화하기 실습 중, 코드 진행이 되지 않아 챕터 종료

--- section_29/src/components/Greeting.test.js
+++ section_29/src/components/Greeting.test.js
@@ -6,16 +6,21 @@
 import Greeting from "./Greeting";
 import { render, screen } from "@testing-library/react"
 
-test("test Hello World!", () => {
-  // 준비
-  render(<Greeting />);
+// 'Greeting test'에 속하는 test들을 익명함수로 받는다
+// 하나의 suite에 여러 test가 온다
+// describe 가 안되네?
+// describe("Greeting test", () => {
+  test("test Hello World!", () => {
+    // 준비
+    render(<Greeting />);
 
-  // 실행
+    // 실행
 
-  // 단언
-  // 원하는 문구를 찾고 있다면 성공, 없다면 실패
-  // exact를 false로 설정하면, 문구를 감싸는 것은 상관없고 하위 문자열과도 매치
-  // const helloworldElement = screen.getByText("hello world", {exact: false}); <- 먹히지 않는다
-  const helloworldElement = screen.getByText(/hello world/i);
-  expect(helloworldElement).toBeInTheDocument();
-});
+    // 단언
+    // 원하는 문구를 찾고 있다면 성공, 없다면 실패
+    // exact를 false로 설정하면, 문구를 감싸는 것은 상관없고 하위 문자열과도 매치
+    // const helloworldElement = screen.getByText("hello world", {exact: false}); <- 먹히지 않는다
+    const helloworldElement = screen.getByText(/hello world/i);
+    expect(helloworldElement).toBeInTheDocument();
+  });
+// });
