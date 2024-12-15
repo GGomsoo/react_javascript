fec0bd3f4c926db17533b4b1115fdf142d95e472
Park Jong Hyuk <occultism22@naver.com>
2024-12-15 16:26:56 +0900

    493. 진입 애니메이션 추가하기

--- section_26/CSS-practice/src/components/Modal.jsx
+++ section_26/CSS-practice/src/components/Modal.jsx
@@ -1,13 +1,18 @@
 import { createPortal } from 'react-dom';
+import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog 
+      open 
+      className="modal"
+      initial={{ opacity: 0, y: -30 }} // 요소(modal)의 시작 상태를 지정할 수 있는 프로퍼티
+      animate={{ opacity: 1, y: 0 }}>
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
