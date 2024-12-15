1cc118aa7276c07146659da6e0293906618ce727
Park Jong Hyuk <occultism22@naver.com>
2024-12-15 17:08:23 +0900

    494. 요소가 사라지는/삭제되는 애니메이션 넣기

--- section_26/CSS-practice/src/components/Modal.jsx
+++ section_26/CSS-practice/src/components/Modal.jsx
@@ -1,19 +1,23 @@
-import { createPortal } from 'react-dom';
+import { createPortal } from "react-dom";
 import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <motion.dialog 
-      open 
-      className="modal"
-      initial={{ opacity: 0, y: -30 }} // 요소(modal)의 시작 상태를 지정할 수 있는 프로퍼티
-      animate={{ opacity: 1, y: 0 }}>
+      <motion.dialog
+        // initial: 요소(modal)의 시작 상태를 지정할 수 있는 프로퍼티
+        // exit: 요소를 닫을 때 적용할 애니메이션
+        open
+        className="modal"
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 30 }}
+      >
         <h2>{title}</h2>
         {children}
       </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
