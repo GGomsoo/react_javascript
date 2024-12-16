c61f3aefd0acf85fac0149565dd94d56b085cf13
Park Jong Hyuk <occultism22@naver.com>
2024-12-17 02:34:51 +0900

    506. 스크롤 기반 애니메이션

--- section_26/CSS-practice/src/pages/Welcome.jsx
+++ section_26/CSS-practice/src/pages/Welcome.jsx
@@ -1,24 +1,49 @@
 import { Link } from 'react-router-dom';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 import cityImg from '../assets/city.jpg';
 import heroImg from '../assets/hero.png';
 
 export default function WelcomePage() {
+  // useScroll()는 스크롤 위치를 추적하는 훅
+  const { scrollY } = useScroll();
+
+  // useTransform()은 스크롤 위치를 변환하는 훅
+  // 변환 되어야 하는 값, 중단점으로 구성된 배열, 중단점들 사이에 있는 값들이 변환되어야 하는 값
+  // 스크롤 내릴수록 점점 투명해진다
+  const yCity = useTransform(scrollY, [0, 200], [0, -100]);
+  const opacityCity = useTransform(scrollY, [0, 200, 300, 500], [1, 0.5, 0.5, 0]);
+
+  const yHero = useTransform(scrollY, [0, 200], [0, -150]);
+  const opacityHero = useTransform(scrollY, [0, 300, 500], [1, 1, 0]);
+
+  const yText = useTransform(scrollY, [0, 200, 300, 500], [0, 50, 50, 300]);
+  const scaleText = useTransform(scrollY, [0, 300], [1, 1.5]);
+
   return (
     <>
       <header id="welcome-header">
-        <div id="welcome-header-content">
+        <motion.div id="welcome-header-content"
+        style={{ scale: scaleText, y: yText }}>
           <h1>Ready for a challenge?</h1>
           <Link id="cta-link" to="/challenges">
             Get Started
           </Link>
-        </div>
-        <img
+        </motion.div>
+
+        {/* 스크롤에 따른 투명도 설정 */}
+        <motion.img
+          style={{ opacity: opacityCity, y: yCity }}
           src={cityImg}
           alt="A city skyline touched by sunlight"
           id="city-image"
         />
-        <img src={heroImg} alt="A superhero wearing a cape" id="hero-image" />
+        <motion.img 
+          style={{ opacity: opacityHero, y: yHero }}
+          src={heroImg} 
+          alt="A superhero wearing a cape" 
+          id="hero-image" 
+        />
       </header>
       <main id="welcome-content">
         <section>
