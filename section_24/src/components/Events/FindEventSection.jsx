769d62323dcdd0bee9ad538d26be9b99487ff3d6
Park Jong Hyuk <occultism22@naver.com>
2024-12-17 19:15:08 +0900

    392. 동적 쿼리 함수 및 쿼리 키

--- section_24/src/components/Events/FindEventSection.jsx
+++ section_24/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,53 @@
-import { useRef } from 'react';
+import { useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchEvents from "../../util/http";
+
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorBlock from "../UI/ErrorBlock";
+import EventItem from "./EventItem";
 
 export default function FindEventSection() {
+  // 현재 검색중인 일부 결과만을 얻고 싶다
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState();
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  }
+
+  // queryFn을 익명 함수로 설정, 검색 키워드를 포함한 결과를 얻고 싶다
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { search: searchTerm }],
+    queryFn: () => fetchEvents(searchTerm),
+  });
+
+  let content = <p>Loading...</p>;
+
+  // 시나리오에 따른 content 값을 설정하였음.
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events"}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
@@ -20,7 +63,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
--- section_24/src/components/Events/NewEventsSection.jsx
+++ section_24/src/components/Events/NewEventsSection.jsx
@@ -10,6 +10,7 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events'],
     queryFn: fetchEvents,
+    staleTime: 1000 * 5,
   });
 
   let content;
