d0266c09ca43b918b905109622e420b9617b4fe3
Park Jong Hyuk <occultism22@naver.com>
2024-12-17 20:38:50 +0900

    401. 데모 앱 개선 및 변형 개념 반복

--- section_24/src/components/Events/EventDetails.jsx
+++ section_24/src/components/Events/EventDetails.jsx
@@ -1,8 +1,98 @@
-import { Link, Outlet } from 'react-router-dom';
-
-import Header from '../Header.jsx';
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import Header from "../Header.jsx";
+import { fetchEvent, deleteEvent, queryClient } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  // event 별로 고유한 id 를 가져온다
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["event", id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+
+  // delete 버튼을 눌렀을 때 이벤트를 삭제하는 쿼리
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      navigate("/events");
+      // 변형이 성공됐을 때
+      // 현재 화면에 표시된 컴포넌트와 관련된 쿼리가 실행된 경우
+      // 특정 쿼리로 가져온 데이터가 오래됬으니 만료로 표시
+      // 즉시 다시 가져오라고 쿼리에 알린다
+
+      // queryKey 가 포함된 쿼리는 무효화된다.
+      // exact 옵션을 사용하면, queryKey 가 정확히 일치하는 쿼리만 무효화된다.
+      queryClient.invalidateQueries({ 
+        queryKey: ['events'],
+        
+        // 자동으로 트리거되지 않도록 해준다.
+        refetchType: "none",
+       });
+    },
+  });
+
+  const handleDelete = () => {
+    mutate({ id });
+  };
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Fetching event data...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title="Failed to fetch event"
+          message={error.info?.message || "Failed to fetch event"}
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString('kr-kr', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content" className="center">
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt="" />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {formattedDate} @ {data.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +101,7 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
+      <article id="event-details">{content}</article>
     </>
   );
 }
